b54344280c1217d238de46b3cbab52a45069beba
shaikmohammedyasin-create <nottouchmymail@gmail.com>
2026-08-04 18:24:42 +0530

    Update find-numbers-with-even-number-of-digits.java

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -1,14 +1,14 @@
 class Solution {
     public int findNumbers(int[] nums) {
-        int ans = findeven(nums);
-        // System.out.println(ans);
-        return ans;
-    }
+        int evendigitcount = findeven(nums); 
+        return evendigitcount;
+        }
+
 
     static int findeven(int[] nums){
         int count = 0;
-        for(int i = 0;i<nums.length;i++){
-            if(evenn(nums[i])){
+        for(int i =0;i<nums.length;i++){
+            if(counteven(nums[i])){
                 count++;
             }
 
@@ -16,28 +16,25 @@ class Solution {
         return count;
 
 
+
     }
 
-    static boolean evenn(int number){
-        int NumbEven = digits(number);
-        int Evee = NumbEven % 2;
-        if(Evee==0){
+    static boolean counteven(int countnumbers){
+        int Numberofdigits = digits(countnumbers);
+        if(Numberofdigits % 2 == 0 )
+        {
             return true;
         }
         return false;
     }
 
-    static int digits(int number){
+    static int digits(int countnumbers){
         int count = 0;
-        while(number > 0){
-        
-        number = number/10;
-        count++;
-
+        while(countnumbers>0){
+            countnumbers = countnumbers/10;
+            count++;
         }
-
         return count;
-
     }
-}
 
+}
