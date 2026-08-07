0cfb16262d866734eb7c6cf29dde7a1a5398913d
shaikmohammedyasin-create <nottouchmymail@gmail.com>
2026-08-07 07:25:18 +0530

    Time: 0 ms (100.00%) | Memory: 80.2 MB (59.85%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int ans = peakindex(arr);
+        return ans;
+    }
+
+
+    public int peakindex(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+
+
+        //for traveersing of the arrya we are using the while loop
+
+        while(start < end){
+            //formulae for calculating the mid value
+            int mid = start + (end - start)/2;
+
+            //here we are checking that in that arrray we if the mid value is greater than next elemetn of the mid value in the arrya then we are in the descending phase so  then we need to assing the end value as the mid so the next part of the areray from the mid is the ascendign phase so then it shift
+            
+            if(arr[mid] > arr[mid + 1]){
+                end = mid;
+                
+            }
+            //else ids used to if the opposite of the above condiiton is then we are in the ascending so we need to assogn the start value as the start = mid + 1 so that menas we are in ascendiing order
+            else{
+                start = mid + 1;            }
+        }
+    return start;
+
+    }
+    // return start cuase the actaully required answer will be in the ascending order phase we after mutilple iteraitons and calculations of the mid in the arr so we finally in the else becomes true wheer the mid value is lesss than the netx vlaue of the next midd so thats the point we want to find so here start acts as index value so finally the asnwer is here bleow
+}
